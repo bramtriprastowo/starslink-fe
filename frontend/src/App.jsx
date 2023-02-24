3b4ba742eac766a360bb91819dd7962f4b649ba9
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2023-02-24 16:39:16 +0700

    add body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
+      <Navbar />
+      <Body />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
